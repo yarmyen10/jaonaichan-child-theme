eebc829d7bf3c3fa776c167f3a8c5426d7bcc574
yarmyen <Phongsathon.yen@gmail.com>
2026-03-19 20:08:55 +0700

    refactor: simplify textdomain loading function and remove debug info

--- src/inc/i18n/languages.php
+++ src/inc/i18n/languages.php
@@ -2,23 +2,12 @@
 /**
  * Language & i18n Setup
  */
-
-/**
- * โหลดไฟล์ภาษาของ Theme
- * ไฟล์ .mo จะอยู่ที่ /src/inc/i18n/languages/
- */
 function theme_load_textdomain() {
-    $mo_file = get_stylesheet_directory() . '/src/inc/i18n/languages/' 
-               . $_ENV['TEXTDOMAIN_NAME'] . '-' . get_locale() . '.mo';
-
-    $result = load_textdomain( $_ENV['TEXTDOMAIN_NAME'], $mo_file );
-
-    do_action('qm/info', 'mo_file: ' . $mo_file);
-    do_action('qm/info', 'load_textdomain result: ' . ( $result ? 'YES' : 'NO' ));
-
-    // เช็ค global $l10n ว่า textdomain ถูกลงทะเบียนไหม
-    global $l10n;
-    do_action('qm/info', 'l10n has domain: ' . ( isset($l10n[$_ENV['TEXTDOMAIN_NAME']]) ? 'YES' : 'NO' ));
+    load_textdomain(
+        $_ENV['TEXTDOMAIN_NAME'],
+        get_stylesheet_directory() . '/src/inc/i18n/languages/'
+            . $_ENV['TEXTDOMAIN_NAME'] . '-' . get_locale() . '.mo'
+    );
 }
 add_action( 'init', 'theme_load_textdomain', 1 );
 
