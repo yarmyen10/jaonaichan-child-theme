e15a70938dd100249fbbfdb5509f07af85558d3a
yarmyen <Phongsathon.yen@gmail.com>
2026-03-19 20:27:35 +0700

    ok 👌 : set languages

--- src/inc/i18n/languages.php
+++ src/inc/i18n/languages.php
@@ -3,12 +3,13 @@
  * Language & i18n Setup
  */
 function theme_load_textdomain() {
-    load_child_theme_textdomain(
-        $_ENV['TEXTDOMAIN_NAME'],                                          // textdomain
-        get_stylesheet_directory() . '/src/inc/i18n/languages'     // path ไฟล์ภาษา
+    load_textdomain(
+        $_ENV['TEXTDOMAIN_NAME'],
+        get_stylesheet_directory() . '/src/inc/i18n/languages/'
+            . $_ENV['TEXTDOMAIN_NAME'] . '-' . get_locale() . '.mo'
     );
 }
-add_action( 'after_setup_theme', 'theme_load_textdomain' );
+add_action( 'init', 'theme_load_textdomain', 1 );
 
 
 /**
