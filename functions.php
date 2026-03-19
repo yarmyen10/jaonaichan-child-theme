6508a1f4bde6df394178025ededdb5545129d867
yarmyen <Phongsathon.yen@gmail.com>
2026-03-19 09:45:06 +0000

    hotfix 🔥 : path env

--- functions.php
+++ functions.php
@@ -13,15 +13,15 @@
  */
 define( 'CHILD_THEME_JAO_NAI_CHAN_VERSION', '1.0.0' );
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once get_stylesheet_directory() . '/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable(get_stylesheet_directory());
 $dotenv->load();
 
 $inc_folders = [
     '/src/inc/enqueue/',
     '/src/inc/auth/',
-    // '/src/inc/woocommerce/',
+    '/src/inc/woocommerce/',
     '/src/inc/i18n/',
     // '/src/inc/helpers/',
 ];
