4b0bf05fb9ee1ab6c56408e997123b1a66512b6a
yarmyen10 <phongsathon.yen@gmail.com>
2026-03-20 11:54:36 +0700

    wip 🚧 : dirname(__DIR__, 2) <2

--- functions.php
+++ functions.php
@@ -42,6 +42,7 @@ foreach ( $inc_folders as $folder ) {
 
 add_action('init', function() {
     do_action('qm/info', '__DIR__: ' . __DIR__);
+    do_action('qm/info', '__DIR__ <2: ' . dirname(__DIR__, 2));
     do_action('qm/info', 'locale: ' . get_locale());
     do_action('qm/info', 'WPLANG: ' . get_option('WPLANG'));
     do_action('qm/info', 'translated: ' . __('รอโอนเงิน', $_ENV['TEXTDOMAIN_NAME']));
